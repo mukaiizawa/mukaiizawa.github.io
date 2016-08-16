68cacca9fe3e67aab03110e3da0510836ae477a0
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-08-16 19:16:12 +0900

    Update

--- make.lisp
+++ make.lisp
@@ -2,16 +2,17 @@
 (require "stdlib" *module-stdlib*)
 
 (mapfile (lambda (pathname)
-           (princln pathname)
-           (let ((pathname-html (make-pathname
-                                  :type "html"
-                                  :defaults pathname)))
-             (with-open-file (out pathname-html 
-                                  :direction :output
-                                  :if-exists :supersede
-                                  :if-does-not-exist :create)
-               (let ((*standard-output* out))
-                 (load pathname)))))
+           (unless (find ".git" (pathname-directory pathname) :test equal)
+             (princln pathname)
+             (let ((pathname-html (make-pathname
+                                    :type "html"
+                                    :defaults pathname)))
+               (with-open-file (out pathname-html 
+                                    :direction :output
+                                    :if-exists :supersede
+                                    :if-does-not-exist :create)
+                 (let ((*standard-output* out))
+                   (load pathname))))))
          :extension 'lisp
          :directory "./root"
          :recursive t)
