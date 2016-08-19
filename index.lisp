c8b4bc36e2bf28e8f68882149427d4def72bc68b
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-08-19 15:50:12 +0900

    Update

--- index.lisp
+++ index.lisp
@@ -8,24 +8,37 @@
       (:head
         (:meta ((charset "utf-8")))
         (:meta ((name "viewport") (content "width=device-width")))
+        (:link ((href "resources/fonts/font-awesome-4.6.3/font-awesome.css") (rel "stylesheet")))
         (:link ((href "resources/css/style.css") (rel "stylesheet"))))
       (:body
         (:div ((class "wrapper"))
-          (title)
-          (:div ((class "boxA"))
-            (:div ((class "box3"))
-              (:article ((class "content"))
-                (:h1 "もぎたてトマトをおいしく食べたい")
-                (:p
-                  "もぎたてのトマトを食べたくなって，ちょっと郊外にあるトマトの農家さんの直売所まで行ってきました．
-                  ここのトマトは新鮮で，おいしくて，お安いという，とってもお買い得なトマトなんです．")
-                (:p
-                  "もぎたての新鮮なトマトは，下手が緑色でみずみずしく，シャキっとしてます．
-                  赤い実は皮がピンと張り，つるっと丸いものがいいのだとか．糖度の高いトマトは水に沈むそうですが，
-                  何でも甘ければよいというものではないので，好みの味のトマトを作る農家さんを見つけたいところです．")))
-                  (:div ((class "box4"))
-                    (:aside "box4"
-                      )))
-                  (:div ((class "box5")) "box5")
-                  )))))
+          (:header
+            (:h1
+              (:a ((href "#")) "GoTo Great")
+              (:p "偉大なエンジニアを目指して")))
+          (:div ((class "center-box"))
+            (:div ((class "contents"))
+              (:article
+                (:header
+                  (:span ((class "fa fa-clock-o"))) (:htime ((datetime "2016-08-19")) "2016-08-19")
+                  ; (:span ((class "fa fa-tag")) "html")
+                  )
+                (:h1 "ブログ開設しました")
+                ; (:h2 "h2")
+                (:p "comming soon.")))
+            (:div ((class "sidemenu"))
+              (:aside
+                (:h1 "プロフィール")
+                (:img ((src "resources/images/profile_picture.png") (alt "photo of mukaiizawa") (width "80px") (height "80px;")))
+                (:p "mukaiizawa")
+                (:p (:span ((class "fa fa-github")) "github")))
+              (:aside
+                (:h1 "記事一覧")
+                (:ul
+                  (:li (:a ((href "#")) (:span ((class "fa fa-caret-right")) "page0")))
+                  (:li (:a ((href "#")) (:span ((class "fa fa-caret-right")) "page1")))
+                  (:li (:a ((href "#")) (:span ((class "fa fa-caret-right")) "page2")))
+                  (:li (:a ((href "#")) (:span ((class "fa fa-caret-right")) "page3")))))))
+          (:footer ((class "page"))
+            (:p "Copyright © mukaiizawa")))))))
 
--- module.lisp
+++ module.lisp
@@ -1,6 +1,6 @@
 
-(defmacro title ()
-  `(:div ((class "title"))
+(defmacro header ()
+  `(:header
      (:h1
        (:a ((href "#")) "GoTo Great")
        (:p "偉大なエンジニアを目指して"))))
